2dc3610b79dd5d3b5bee586a7410af5d7de1305b
compound96 <47362332+compound96@users.noreply.github.com>
2021-03-16 14:47:01 +0100

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -1,8 +1,6 @@
 import numpy as np
 from math import *
 
-x = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
-y = [10,14,19,26,27,31,33,35,42,44,50,60]
 def Binary_search(y,x0):
     L = len(y)
     high = L; low = 0 # starting indexes
@@ -24,5 +22,4 @@ def Binary_search(y,x0):
             high = mid - 1
         else:
             low = mid + 1
-print(Binary_search(y,11))
-
+            
